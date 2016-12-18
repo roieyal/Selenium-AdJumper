4f62306fc8872e4e90de2d410187bb451eb6c992
Evgeny Zilber <evgeny.zilber@hpe.com>
2016-12-18 17:11:35 +0200

    created MainClass and added maven plugin for creating executable jar

--- src/main/java/common/WebDriverFactory.java
+++ src/main/java/common/WebDriverFactory.java
@@ -1,4 +1,5 @@
-import common.BrowserType;
+package common;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
